9cf93e09d748c6f6d01e9ee6cb12dd160385d50d
Alibek Suleimenov <alibek443@gmail.com>
2024-03-07 18:56:30 +0500

    initial commit: go web app with stripe payments

---
+++ cmd/web/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"encoding/gob"
+	"flag"
+	"fmt"
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
+	"html/template"
+	"log"
+	"myapp/internal/driver"
+	"myapp/internal/models"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+const cssVersion = "1"
+
+var session *scs.SessionManager
+
+type config struct {
+	port int
+	env  string
+	api  string
+	db   struct {
+		dsn string
+	}
+	stripe struct {
+		secret string
+		key    string
+	}
+	secretkey string
+	frontend  string
+}
+
+type application struct {
+	config        config
+	infoLog       *log.Logger
+	errorLog      *log.Logger
+	templateCache map[string]*template.Template
+	version       string
+	DB            models.DBModel
+	Session       *scs.SessionManager
+}
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port),
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
+
+	app.infoLog.Println(fmt.Sprintf("Starting HTTP server in %s on port %d", app.config.env, app.config.port))
+
+	return srv.ListenAndServe()
+}
+func main() {
+	gob.Register(TransactionData{})
+
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production}")
+	flag.StringVar(&cfg.db.dsn, "dsn", "alikawp:secret@tcp(localhost:3306)/widgets?parseTime=true&tls=false", "dsn")
+	flag.StringVar(&cfg.api, "api", "http://localhost:4001", "URL to API")
+	flag.StringVar(&cfg.secretkey, "secret", "f291e6450b907e4d1bd79bfefb640a4b", "secret key")
+	flag.StringVar(&cfg.frontend, "frontend", "http://localhost:4000", "url to front end")
+
+	flag.Parse()
+
+	cfg.stripe.key = os.Getenv("STRIPE_KEY")
+	cfg.stripe.secret = os.Getenv("STRIPE_SECRET")
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	conn, err := driver.OpenDB(cfg.db.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer conn.Close()
+
+	// set up session
+	session = scs.New()
+	session.Lifetime = time.Hour * 24
+	session.Store = mysqlstore.New(conn)
+
+	tc := make(map[string]*template.Template)
+
+	app := &application{
+		config:        cfg,
+		infoLog:       infoLog,
+		errorLog:      errorLog,
+		templateCache: tc,
+		version:       version,
+		DB: models.DBModel{
+			DB: conn,
+		},
+		Session: session,
+	}
+
+	//go app.ListenToWsChannel()
+
+	err = app.serve()
+	if err != nil {
+		app.errorLog.Println(err)
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,23 @@
+module myapp
+
+go 1.21.5
+
+require golang.org/x/crypto v0.18.0
+
+require (
+	github.com/alexedwards/scs/mysqlstore v0.0.0-20231113091146-cef4b05350c8 // indirect
+	github.com/alexedwards/scs/v2 v2.7.0 // indirect
+	github.com/bwmarrin/go-alone v0.0.0-20190806015146-742bb55d1631 // indirect
+	github.com/go-chi/chi/v5 v5.0.11 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/phpdave11/gofpdf v1.4.2 // indirect
+	github.com/phpdave11/gofpdi v1.0.12 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stripe/stripe-go/v72 v72.122.0 // indirect
+	github.com/toorop/go-dkim v0.0.0-20201103131630-e1cd1a0a5208 // indirect
+	github.com/xhit/go-simple-mail/v2 v2.16.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+)
